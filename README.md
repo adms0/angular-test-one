928b1dceb2d995602466211e9253fb2ffaddd48e
adly_maulana <adly.ms@gmail.com>
2024-09-28 23:50:38 +0700

    [DEV] initial development with initial slicing

--- README.md
+++ README.md
@@ -6,6 +6,18 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
 
+## Angular Material 
+Material Design components for Angular for fasting development, will use
+Material Design `https://v7.material.angular.io/guide/getting-started`. chose the `ng add` for automatically update the Angular project with 
+the correct dependencies set up based on the needs like custom theme, 
+enabled animation etch.
+
+## Tailwind CSS for Angular
+Tailwind CSS go to the website `https://tailwindcss.com/` for customize
+component and could adapts with any design. click to get started with 
+Tailwind CSS and install with `npm install -D tailwindcss` at your 
+terminal and create config file with `npx tailwindcss init`
+
 ## Code scaffolding
 
 Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterViewInit, ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatDivider } from '@angular/material/divider'
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout'
+import { NgIf } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { MatList, MatListItem, MatNavList } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatDivider,
+    MatIconModule,
+    NgIf,
+    RouterModule,
+    CommonModule,
+    MatNavList,
+    MatList,
+    MatListItem
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+
+export class AppComponent implements AfterViewInit {
+
   title = 'news-app';
+  articles: any = [];
+  sources: any = [];
+  @ViewChild(MatSidenav)
+  sideNav!: MatSidenav;
+  constructor(private observer: BreakpointObserver, private cdr: ChangeDetectorRef) { }
+  ngAfterViewInit() {
+    this.observer.observe(["(max-width: 800px)"]).subscribe((res) => {
+      if (res.matches) {
+        this.sideNav.mode = "over";
+        this.sideNav.close();
+      } else {
+        this.sideNav.mode = "side";
+        this.sideNav.open();
+      }
+    });
+    this.cdr.detectChanges();
+  }
+
 }
